a07e74e557f4807f637e6ddd189ff378e76a410f
inclyc <me@inclyc.cn>
2022-07-25 01:25:45 +0800

    random_tree_test

--- test/Heuristic/test.cpp
+++ test/Heuristic/test.cpp
@@ -1,6 +1,7 @@
 #include "Heuristic/ColorGraph.h"
 #include "Heuristic/Graph.h"
 #include <cassert>
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <set>
@@ -28,19 +29,27 @@ static void exit_test() {
   std::cout << test_name << " passed.\n";
 }
 
-static void random_tree() {
-  auto r = Heuristic::random_bigraph(10, 9);
-  auto a = std::set<int>();
-  auto b = std::set<int>();
-  a.insert(1);
-  b.insert(4);
-  auto pp = std::make_unique<Heuristic::ColorGraph>(std::move(*r), std::move(a),
-                                                    std::move(b));
-  pp->bfs();
-  pp->connection();
+static void random_tree_test() {
+  const std::string test_name("random_tree_test");
+  std::cout << test_name << " ...\n";
+
+  for (int i = 0; i < 1000; i++) {
+    int n = rand() % 1000 + 20;
+    auto g = Heuristic::random_bigraph(n, n - 1);
+    auto a = std::set<int>();
+    auto b = std::set<int>();
+    a.insert(1);
+    b.insert(4);
+    auto hcg = std::make_unique<Heuristic::ColorGraph>(
+        std::move(*g), std::move(a), std::move(b));
+    hcg->bfs();
+    assert(hcg->validate());
+  }
+
+  std::cout << test_name << " passed.\n";
 }
 
 int main() {
   exit_test();
-  random_tree();
+  random_tree_test();
 }
