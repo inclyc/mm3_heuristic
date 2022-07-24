98af88b2ec900299ee5887e7463f1ef5f25a9bd1
inclyc <me@inclyc.cn>
2022-07-24 18:06:00 +0800

    moves dynamic graph test to splited directory

--- test/xmake.lua
+++ test/xmake.lua
@@ -1,8 +1,2 @@
 includes("DSU")
-
-add_rules("mode.debug", "mode.release")
-target("test")
-    set_kind("binary")
-    add_includedirs("$(projectdir)/include")
-    add_files("*.cpp")
-    add_deps("DynamicGraph")
+includes("DynamicGraph")
