8080818c4c0e1304f5ba2d3323ad3940f5270996
YingChi Long <me@inclyc.cn>
2022-08-29 15:24:38 +0800

    [Heuristic] modify bench to estimate new algorithm
    If we specify TestNum = 0, member function `spanningTree` was fallback
    to MST, that's exactly our approach before.

--- test/Heuristic/BenchRandom.cpp
+++ test/Heuristic/BenchRandom.cpp
@@ -10,16 +10,18 @@ int main(int argc, char *argv[]) {
   }
   fmt::print("Random rounds: {}\n", TestNum);
   for (int TestCase = 15; TestCase <= 100; TestCase++) {
-    auto Average = [&](int Case, int EdgeNum) -> std::pair<float, float> {
-      float Sum1 = 0, Sum2 = 0;
+    auto Average = [&](int Case,
+                       int EdgeNum) -> std::tuple<float, float, float> {
+      float Sum1 = 0, Sum2 = 0, Sum3 = 0;
       for (int I = 0; I < Case; I++) {
         auto G = Heuristic::randomBiGraph<Heuristic::MSTGraph>(15, EdgeNum);
         Sum1 += G->bruteForce().first;
-        Sum2 += G->solve(TestNum).first;
+        Sum2 += G->solve(1).first; // i.e. don't shuffle
+        Sum3 += G->solve(TestNum).first;
       }
-      return std::make_pair(Sum1, Sum2);
+      return std::make_tuple(Sum1, Sum2, Sum3);
     };
-    auto [A, B] = Average(10, TestCase);
-    fmt::print("{} {}\n", A, B);
+    auto [A, B, C] = Average(10, TestCase);
+    fmt::print("{} {} {}\n", A, B, C);
   }
 }
